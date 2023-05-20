32e795bea791ea78eb304daac67feec88e34e4e9
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-20 15:10:14 +0200

    implementing the exit cmd

---
+++ exit.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * exitShell - exit cmd implementation
+ */
+
+void exitShell()
+{
+	printf("Exiting the custom shell...\n");
+	/*Add any cleanup or additional actions before exiting, if needed*/
+	exit(0); /* Terminate the shell program*/
+}
