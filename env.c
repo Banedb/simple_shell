aa4edc3e0d81088bfea4edae6bcf041d364db837
Kayr0r404 <mahlarekarabo.702@gmail.com>
2023-05-20 12:32:46 +0200

    env implementation

---
+++ env.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * env - enviroment implementation
+ * @envStrings: array of pointer to strings storing
+ * environment variable and its values
+ */
+void
+env(char **envStrings)
+{
+	env_t *envs;
+	char **envp;
+	int i, envCount = 0;
+
+	envp = environ;
+
+	/* Count the number of environment variables */
+	for (; *envp != NULL; envCount++, envp++)
+		;
+	envs = (env_t *) malloc(sizeof(struct Env) * envCount);
+	/*handle malloc failure*/
+	if (!envs)
+	{
+		return;
+	}
+
+	/* Populate the environment variables array */
+	envp = environ;
+	for (i = 0; i < envCount; i++)
+	{
+		char *env = *envp;
+		int j = 0;
+
+		while (env[j] != '=')
+			j++;
+
+		envs[i].key = strndup(env, j);
+		envs[i].val = env + j + 1;
+		envp++;
+	}
+
+	/* Store the environment variables in an array of pointers to strings */
+	envStrings = malloc(sizeof(char *) * envCount);
+	if (!envStrings)
+	{
+		return;
+	}
+
+	while ((i = 0) < envCount)
+	{
+		/* +2 for '=' and null terminator*/
+		int length = strlen(envs[i].key) + strlen(envs[i].val) + 2;
+
+		envStrings[i] = malloc(sizeof(char) * length);
+		snprintf(envStrings[i], length, "%s=%s", envs[i].key, envs[i].val);
+	}
+
+	/* Access the environment variables from the array of pointers to strings */
+	/**
+	 * * while (envCount)
+	 * {
+	 * printf("%s\n", envStrings[i]);
+	 * i++;
+	 * }
+	 */
+	/* Free the allocated memory */
+	for (i = 0; i < envCount; i++)
+	{
+		free(envs[i].key);
+		free(envStrings[i]);
+	}
+	free(envs);
+	free(envStrings);
+}
