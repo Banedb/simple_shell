a8af5269ec6a2976229e9affb2c9d6626b844017
Bane <marcytornad@gmail.com>
2023-11-01 07:30:29 -0700

    shell

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Arguments passed to function
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	prompt = "$ ";
+	/*printf("main1 here\n");*/
+	name = argv[0];
+	if (signal(SIGINT, sig_h) == SIG_ERR)
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	/*fflush(stdout);*/
+	run_input();
+	(void)argc;
+	return (0);
+}
